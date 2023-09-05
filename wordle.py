408c2d3f7ab3d856dfbcee35ea15a69de8c30789
torcuata22 <marilynjmarquez@gmail.com>
2023-09-05 12:29:27 -0400

    logic works

--- wordle.py
+++ wordle.py
@@ -1,31 +1,45 @@
 import random
-words = ['apple', 'cherry', 'lemon', 'grape', 'banana']
-word_to_guess= ''
+
+words = ['apple', 'lemon', 'grape', 'knife', 'kitty']
+word_to_guess = random.choice(words)
 attempts = 0
 max_attempts = 6
-guessed_letters = set()
+guessed_letters = []
+guessed_words=set()
 
 while attempts < max_attempts:
-    word_to_guess= random.choice(words)
     guess = input("Guess a word: ").lower()
+
+    if guess in guessed_words:
+        print("You already guessed that word. Try a different one.")
+        continue
+    
     if len(guess) != 5:
         print("Invalid input. Please enter a 5-letter word.")
         continue
-    if len(guess) == 5 and word_to_guess == guess:
-        print(f"Congratulations! You guessed: {word_to_guess}")
+    
+    guessed_words.add(guess)
+
     
     correct_letters = 0
+    correct_letter_positions = []  # Store the positions of correctly guessed letters
+    
     for i in range(5):
         if guess[i] == word_to_guess[i]:
+            guessed_letters.append(guess[i])
+            correct_letter_positions.append(i)
             correct_letters += 1
-            print(f"Correct letters: {correct_letters}/5")
+    
+    # Provide feedback on correct and incorrect letters
+    feedback = ['_' if i not in correct_letter_positions else guess[i] for i in range(5)]
+    print(f"Feedback: {' '.join(feedback)}")
+    print(f"Correct letters: {correct_letters}/5")
 
-        if correct_letters == 5:
-            print(f"Congrtulations! You guessed: {guess}")
-        
-        attempts += 1
+    if correct_letters == 5:
+        print(f"Congratulations! You guessed: {guess}")
+        break
+    
+    attempts += 1
 
-        if attempts >= max_attempts:
-            print(f"Sorry, you've reached the maximum number of attempts. The word was: {word_to_guess}")
-
-
+if attempts >= max_attempts:
+    print(f"Sorry, you've reached the maximum number of attempts. The word was: {word_to_guess}")
